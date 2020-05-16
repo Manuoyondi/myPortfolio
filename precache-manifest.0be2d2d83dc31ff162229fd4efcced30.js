783f26efd593c65f451651de57f7c2e29ef1c497
manu <oyondiimmanuel@hmail.com>
2020-05-16 20:26:37 +0300

    Updates

---
+++ precache-manifest.0be2d2d83dc31ff162229fd4efcced30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff2095409a105cc55d21b2ce63e205b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1d15db2c238dd5cee136",
+    "url": "/portfolio/static/css/main.716be0b1.chunk.css"
+  },
+  {
+    "revision": "80d960db49dd4d8edf30",
+    "url": "/portfolio/static/js/2.302ecd60.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.302ecd60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d15db2c238dd5cee136",
+    "url": "/portfolio/static/js/main.69057a2b.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
